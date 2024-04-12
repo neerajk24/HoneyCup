6711ec03b762a8790c094756cef99594ae43917b
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-04-13 02:29:40 +0530

    done some small updation in  app.js

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ import authRoutes from './src/api/routes/auth.route.js'; // Update the import st
 import userRoutes from './src/api/routes/user.route.js'; // Update the import statement to include the file extension
 import mediaRoutes from './src/api/routes/media.route.js'; // Update the import statement to include the file extension
 
-import cors from 'cors'; // Import the cors package
+import passport from 'passport';
+import './src/config/passport-setup.js';
+import cors from 'cors'; 
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import multer from 'multer';
 
 const app = express();
 
@@ -28,9 +31,11 @@ connectDatabase();
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(json());
 app.use(cors()); 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/media', mediaRoutes);
+app.use(passport.initialize());
 
 // ... other middleware
 
@@ -44,7 +49,8 @@ const swaggerOptions = {
         description: 'Documentation for the HoneyCup API',
       },
     },
-    // Note: Adjust the path to where your route definitions are located
+    // Note: Adjusted path to location where your route located
+    // handle all .js file in ./src/api/routes/
     apis: ['./src/api/routes/*.js'],
   };
   
