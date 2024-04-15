1f3cb1f294f5aacd225ad20382081b069c65bc40
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-04-16 02:14:08 +0530

    correctly locating firebase.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
 import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
-import firebaseApp from "../firebase.js"; // Import the app instance
+import firebaseApp from "../config/firebase.js"; // Import the app instance
 
 // Module-level variable to store initialized Firebase instance
 let auth;
