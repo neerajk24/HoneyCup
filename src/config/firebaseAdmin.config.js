ef6a6d8470474d8d16ea960fd519308f2001e1db
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-04-21 23:59:27 +0530

    Updated , need to replace env. variable

--- src/config/firebaseAdmin.config.js
+++ src/config/firebaseAdmin.config.js
@@ -1,8 +1,25 @@
-import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json'; // Adjust the path as necessary
+// src/config/firebaseAdmin.config.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyChrFIxe5YfCfIRD6Fzfq0z5NVWJL8epqY",
+    authDomain: "transition-chat.firebaseapp.com",
+    projectId: "transition-chat",
+    storageBucket: "transition-chat.appspot.com",
+    messagingSenderId: "1042379704022",
+    appId: "1:1042379704022:web:d0cd900df0b1b8b3a5ab77",
+    measurementId: "G-XWGR05CMXP"
+};
 
-export default admin;
+const app = initializeApp(firebaseConfig);
+let analytics;
+
+if (typeof window !== "undefined" && getAnalytics.isSupported()) {
+  analytics = getAnalytics(app);
+} else {
+  console.warn("Firebase Analytics is not supported in this environment.");
+}
+
+export default app;
+export { analytics };
