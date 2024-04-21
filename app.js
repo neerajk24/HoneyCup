efad510f9618ba0548cb1a2b7afa2b327b676caa
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-04-22 00:06:20 +0530

    Update nothing much

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config();
+/**
+ * This is the main file for the server application.
+ * It sets up the server, connects to the database, and defines routes and middleware.
+ */
+// app.js or server.js
 
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import socketioConfig from './src/config/socketio.config.js';
-import connectDatabase from './src/config/database.js';
-import authRoutes from './src/api/routes/auth.route.js';
-import userRoutes from './src/api/routes/user.route.js';
-import mediaRoutes from './src/api/routes/media.route.js';
-import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import express, { json } from 'express';
 import connectDatabase from './src/config/database.js'; // Update the import statement to include the file extension
@@ -20,18 +17,15 @@ import mediaRoutes from './src/api/routes/media.route.js'; // Update the import 
 import passport from 'passport';
 import './src/config/passport-setup.js';
 import cors from 'cors'; 
-
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import multer from 'multer';
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
 
+// Connect to MongoDB
 connectDatabase();
-socketioConfig(io);
-
+ 
 
 // Middleware setup
 app.use(express.json()); // Middleware to parse JSON bodies
@@ -43,24 +37,35 @@ app.use('/api/users', userRoutes);
 app.use('/api/media', mediaRoutes);
 app.use(passport.initialize());
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'HoneyCup API',
-      version: '1.0.0',
-      description: 'Documentation for the HoneyCup API',
-    },
+// ... other middleware
 
+// Swagger setup
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'HoneyCup API',
+        version: '1.0.0',
+        description: 'Documentation for the HoneyCup API',
+      },
+    },
     // Note: Adjusted path to location where your route located
     // handle all .js file in ./src/api/routes/
     apis: ['./src/api/routes/*.js'],
   };
   
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  
 
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+// Routes setup
+app.use('/api', authRoutes);
+// ... other routes
+
+
+// ... rest of the server setup
+// At the bottom of app.js or server.js
 export default app;
