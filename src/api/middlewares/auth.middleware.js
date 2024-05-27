b38b03b0757329ffb7a37f83f8a1c3d2bfcb30b4
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-05-28 01:55:45 +0530

    Update auth.middleware.js

--- src/api/middlewares/auth.middleware.js
+++ src/api/middlewares/auth.middleware.js
@@ -1,16 +1,15 @@
-// /src/api/middlewares/auth.middleware.js
-import admin from '../../config/firebaseAdmin.config.js';
+// src/api/middlewares/auth.middleware.js
+import admin from '../../config/firebaseAdmin.config.jest.js';
 
 export const verifyToken = async (req, res, next) => {
-  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-    return res.status(403).send('Unauthorized');
-  }
-
-  const token = req.headers.authorization.split('Bearer ')[1];
   try {
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+      return res.status(403).send('Unauthorized');
+    }
+
+    const token = req.headers.authorization.split('Bearer ')[1];
     const decodedToken = await admin.auth().verifyIdToken(token);
     console.log('Token Verified!', decodedToken);
-    // Attach decodedToken to request for further use
     req.user = decodedToken;
     next();
   } catch (error) {
