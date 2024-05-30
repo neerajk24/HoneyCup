4fece90e050e62a60c051a71d30e2abf1c3d5882
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-05-30 21:09:03 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,29 @@
 // HoneyCup/src/config/database.js
 import { connect } from 'mongoose';
 import dotenv from 'dotenv';
+import publicIp from 'public-ip';
+
 dotenv.config();
 
-console.log('Connecting to MongoDB at:', process.env.MONGODB_URI );   // mongodb://localhost:27017/honeyCup
-
 const connectDatabase = async () => {
-    // Determine the connection URI based on the environment
     const dbUri = process.env.MONGODB_URI; // mongodb://localhost:27017/honeyCup
 
-  try {
-    await connect(dbUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    try {
+        const ip = await publicIp.v4();
+        console.log('Connecting to MongoDB at:', dbUri);
+        console.log('Attempting to connect from IP:', ip);
 
-    console.log('MongoDB connected successfully.');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+        await connect(dbUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB connected successfully.');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        // Exit process with failure
+        process.exit(1);
+    }
 };
 
 export default connectDatabase;
