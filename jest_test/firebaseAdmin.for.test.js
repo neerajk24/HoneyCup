25b35d3f03419ac21d975c46b0e55c1678811b45
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-06-07 19:21:29 +0530

    Update firebaseAdmin.for.test.js

--- jest_test/firebaseAdmin.for.test.js
+++ jest_test/firebaseAdmin.for.test.js
@@ -1,23 +1,31 @@
-// jest_test/firebaseAdmin.for.jest_test.js
-import admin from 'firebase-admin';
-import path from 'path';
-import { readFileSync, existsSync } from 'fs';
+// src/config/firebaseAdmin.config.jest.js
+
+import admin from "firebase-admin";
+import path from "path";
+import { readFileSync, existsSync } from "fs";
 
 let adminInitialized = false;
-const serviceAccountPath = path.resolve(__dirname, '../src/config/serviceAccountKey.json');
+const serviceAccountPath = path.resolve(
+  __dirname,
+  "../src/config/serviceAccountKey.json"
+);
 
 if (existsSync(serviceAccountPath)) {
-  const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+  const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.MONGODB_URI
+    databaseURL: process.env.MONGODB_URI,
   });
 
   adminInitialized = true;
-  console.log('-> Firebase Admin SDK has been initialized. We got the serviceAccountKey.json data')
+  console.log(
+    "-> Firebase Admin SDK has been initialized. We got the serviceAccountKey.json data"
+  );
 } else {
-  console.warn('Firebase Admin SDK not initialized. Service account key file not found.');
+  console.warn(
+    "Firebase Admin SDK not initialized. Service account key file not found."
+  );
 }
 
 export default admin;
