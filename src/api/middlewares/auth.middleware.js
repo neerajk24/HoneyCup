77e6bb64981bf16ae9a626fbf70e71100d4b44f2
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-06-08 14:16:20 +0530

    Update auth.middleware.js

--- src/api/middlewares/auth.middleware.js
+++ src/api/middlewares/auth.middleware.js
@@ -1,7 +1,5 @@
 // src/api/middlewares/auth.middleware.js
 
-import admin from "../../../jest_test/firebaseAdmin.for.test.js";
-
 export const verifyToken = async (req, res, next) => {
   try {
     if (
