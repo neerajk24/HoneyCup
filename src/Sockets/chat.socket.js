00f47a8dc5df7f150ace8da9c3b2827250b05f9e
Shriram Bhardwaj <shrirambhardwaj68@gmail.com>
2024-06-19 13:36:44 +0530

    branch merged

--- src/Sockets/chat.socket.js
+++ src/Sockets/chat.socket.js
@@ -1,114 +1,35 @@
 import Conversation from "../models/chats.model.js";
-import mongoose from "mongoose";
-import axios from "axios";
-import ChatService from "../services/chats.service.js";
+import mongoose from 'mongoose';
+import axios from 'axios';
+
 
 let ConnectedSockets = [
   // {Userid , socketId}
-];
+]
 
 export const ChatSocket = (io) => {
-<<<<<<< HEAD
-    io.on('connection', (socket) => {
-        const Userid = socket.handshake.auth.userid;
-        ConnectedSockets.push({
-            socketId: socket.id,
-            Userid
-        })
-        console.log(`${Userid} got connected..`);
-        io.emit('onlineUsers', ConnectedSockets.map(s => s.Userid));
-        socket.on('joinRoom', async ({ userId, conversationId }) => {
-            console.log(`User ${userId} trying to join the room ${conversationId}`);
-            socket.join(conversationId);
-            // Convert the conversationId string to an ObjectId
-            const conversationObjectId = new mongoose.Types.ObjectId(conversationId);
-            const chat = await Conversation.findOne({ _id: conversationObjectId });
-            if (chat) {
-                socket.emit('previousMessages', chat.messages);
-            }
-        });
-=======
-  io.on("connection", (socket) => {
+  io.on('connection', (socket) => {
     const Userid = socket.handshake.auth.userid;
     ConnectedSockets.push({
       socketId: socket.id,
-      Userid,
-    });
+      Userid
+    })
     console.log(`${Userid} got connected..`);
-
-    socket.on("joinRoom", async ({ userId, conversationId }) => {
+    io.emit('onlineUsers', ConnectedSockets.map(s => s.Userid));
+    socket.on('joinRoom', async ({ userId, conversationId }) => {
       console.log(`User ${userId} trying to join the room ${conversationId}`);
       socket.join(conversationId);
       // Convert the conversationId string to an ObjectId
       const conversationObjectId = new mongoose.Types.ObjectId(conversationId);
-
       const chat = await Conversation.findOne({ _id: conversationObjectId });
       if (chat) {
-        socket.emit("previousMessages", chat.messages);
+        socket.emit('previousMessages', chat.messages);
       }
     });
->>>>>>> 9c3d2c1cd941fa5300cb3a2ce4a7830d6410e34f
 
-    socket.on("sendMessages", async ({ conversationId, message }) => {
-      console.log(
-        `${conversationId} is trying to send ${JSON.stringify(
-          message,
-          null,
-          2
-        )}`
-      );
-
-<<<<<<< HEAD
-            // Check if both users are online in the room
-            const usersInRoom = io.sockets.adapter.rooms.get(conversationId);
-            const isReceiverOnline = usersInRoom && usersInRoom.size === 2;
-            // Set is_read to true if both users are online
-            message.is_read = isReceiverOnline;
-            const chat = await Conversation.findOne({ _id: conversationObjectId });
-            if (chat) {
-                chat.messages.push(message);
-                await chat.save();
-            } else {
-                console.log("Error in sending messages Chat not found!");
-            }
-            //send the new unreadMsg to the disconnected User
-            if (!isReceiverOnline) {
-                console.log("oops user hasn't seen messages");
-                const SOCKET = ConnectedSockets.find((soc) => soc.Userid === message.receiver_id);
-                if(SOCKET){
-                    console.log("unseen user found");
-                    const response = await axios.get(`http://localhost:3000/api/socketChat/chats/getUnreadmsg/${message.receiver_id}`);
-                    io.to(SOCKET.socketId).emit('unreadMessages',response.data);
-                    console.log("Unread data send to the user");
-                }
-            }
-        });
-
-        socket.on('userTyping', ({ conversationId, receiverId, typing }) => {
-            const usersInRoom = io.sockets.adapter.rooms.get(conversationId);
-            const isReceiverOnline = usersInRoom && usersInRoom.size === 2;
-    
-            if (isReceiverOnline) {
-                const receiverSocket = ConnectedSockets.find((soc) => soc.Userid === receiverId);
-                if (receiverSocket) {
-                    io.to(receiverSocket.socketId).emit('typing', typing);
-                }
-            }
-        });
-
-        socket.on('disconnect', () => {
-            ConnectedSockets = ConnectedSockets.filter((soc) => soc.Userid !== Userid);
-            io.emit('onlineUsers', ConnectedSockets.map(s => s.Userid));
-            console.log(socket.id + " disconnected");
-        })
-    })
-}
-=======
-      const updatedConversation = await ChatService.sendMessage(
-        conversationId,
-        message
-      );
-      io.to(conversationId).emit("recieveMessage", message);
+    socket.on('sendMessages', async ({ conversationId, message }) => {
+      console.log(`${conversationId} is trying to send ${message}`);
+      io.to(conversationId).emit('recieveMessage', message);
       // Convert the conversationId string to an ObjectId
       const conversationObjectId = new mongoose.Types.ObjectId(conversationId);
 
@@ -127,26 +48,32 @@ export const ChatSocket = (io) => {
       //send the new unreadMsg to the disconnected User
       if (!isReceiverOnline) {
         console.log("oops user hasn't seen messages");
-        const SOCKET = ConnectedSockets.find(
-          (soc) => soc.Userid === message.receiver_id
-        );
+        const SOCKET = ConnectedSockets.find((soc) => soc.Userid === message.receiver_id);
         if (SOCKET) {
           console.log("unseen user found");
-          const response = await axios.get(
-            `http://localhost:3000/api/socketChat/chats/getUnreadmsg/${message.receiver_id}`
-          );
-          io.to(SOCKET.socketId).emit("unreadMessages", response.data);
+          const response = await axios.get(`http://localhost:3000/api/socketChat/chats/getUnreadmsg/${message.receiver_id}`);
+          io.to(SOCKET.socketId).emit('unreadMessages', response.data);
           console.log("Unread data send to the user");
         }
       }
     });
 
-    socket.on("disconnect", () => {
-      ConnectedSockets = ConnectedSockets.filter(
-        (soc) => soc.Userid !== Userid
-      );
-      console.log(socket.id + " disconnected");
+    socket.on('userTyping', ({ conversationId, receiverId, typing }) => {
+      const usersInRoom = io.sockets.adapter.rooms.get(conversationId);
+      const isReceiverOnline = usersInRoom && usersInRoom.size === 2;
+
+      if (isReceiverOnline) {
+        const receiverSocket = ConnectedSockets.find((soc) => soc.Userid === receiverId);
+        if (receiverSocket) {
+          io.to(receiverSocket.socketId).emit('typing', typing);
+        }
+      }
     });
-  });
-};
->>>>>>> 9c3d2c1cd941fa5300cb3a2ce4a7830d6410e34f
+
+    socket.on('disconnect', () => {
+      ConnectedSockets = ConnectedSockets.filter((soc) => soc.Userid !== Userid);
+      io.emit('onlineUsers', ConnectedSockets.map(s => s.Userid));
+      console.log(socket.id + " disconnected");
+    })
+  })
+}
