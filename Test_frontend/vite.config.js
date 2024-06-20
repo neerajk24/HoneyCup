547e8058c932911087620cc62c78b599afcc7f23
Rajat Raj Sharma <139456051+RajatRajSharma@users.noreply.github.com>
2024-06-20 13:47:56 +0530

    Update vite.config.js

--- Test_frontend/vite.config.js
+++ Test_frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createRequire } from "module";
 
-// https://vitejs.dev/config/
+const require = createRequire(import.meta.url);
+
+// Resolve 'crypto' to 'crypto-browserify'
+const cryptoAlias = require.resolve("crypto-browserify");
+
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      crypto: cryptoAlias,
+    },
+  },
+});
